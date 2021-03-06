291821dfd7c5b95b2be217fb0a7178f52f429166
LiQiang <qiangli@thoughtworks.com>
2021-03-06 23:20:11 +0800

    [LiQiang] #n/a feat:init with jira format

---
+++ src/index.ts
@@ -0,0 +1,72 @@
+import inquirer from 'inquirer';
+import process from 'process';
+// import path from 'path';
+// import fs from 'fs';
+// import YAML from 'yamljs';
+
+import longest = require('longest');
+import commitTypes = require('conventional-commit-types');
+import commitizen = require('commitizen');
+import gitconfig = require('gitconfig');
+
+const configLoader = commitizen.configLoader;
+
+var config = configLoader.load() || {};
+
+var length = longest(Object.keys(commitTypes.types)).length + 1;
+
+const types = Object.entries(commitTypes.types)
+  .map(([key, value]) => {
+    return {
+      name: (key + ':').padEnd(length) + ' ' + value.description,
+      value: key
+    }
+  });
+
+// const dir: string = process.cwd();
+//
+// const fileName = '.cz.yml';
+
+// const configFile = path.join(dir, fileName);
+//
+// const load = YAML.load(configFile);
+
+// fs.readFileSync(configFile)
+
+type STEP = ''
+
+const options = {
+  defaultType: process.env.CZ_TYPE || config.defaultType,
+  defaultIssue: process.env.CZ_ISSUE || config.defaultIssue,
+
+};
+
+
+export const prompter = async (cz: any, commit: (msg: string) => void) => {
+  const username = await gitconfig.get({location: 'global'}).then((config) => config.user.name);
+
+  inquirer
+    .prompt([{
+      name: 'issue',
+      type: 'input',
+      message: 'Input jira issue key:',
+      default: options.defaultIssue
+    }, {
+      type: 'list',
+      name: 'type',
+      message: "Select the type of change that you're committing:",
+      choices: types,
+      default: options.defaultType
+    }, {
+      name: 'body',
+      type: 'input',
+      message: 'Input message content:',
+    }])
+    .then(anwser => {
+      commit(`[${username}] #${anwser.issue} ${anwser.type}:${anwser.body}`);
+    })
+};
+
+
+
+
---
+++ src/modules.d.ts
@@ -0,0 +1,29 @@
+declare module 'conventional-commit-types' {
+  export type TypeMap = Record<string, { title: string, description: string }>
+  const types: { types: TypeMap; };
+  export = types;
+
+}
+
+
+declare module 'longest' {
+  function longest(strs: string[]): string
+
+  export = longest;
+}
+
+
+declare module 'commitizen' {
+
+  export const configLoader: { load(): any }
+
+}
+
+declare module 'gitconfig' {
+  interface GitConfig {
+    user: { name: string, email: string; },
+  }
+
+  const config: { get(options: Record<string, string>): Promise<GitConfig>; };
+  export = config
+}
